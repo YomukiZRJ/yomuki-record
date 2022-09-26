b97db4ca5776d8bb0e9043df9b87c9ac155871f4
zengrujing <yomuki@qq.com>
2022-09-26 16:57:15 +0800

    study: ⌨️ cesium

--- packages/vue3-cesium-study/src/libs/cesiumHelper.js
+++ packages/vue3-cesium-study/src/libs/cesiumHelper.js
@@ -1,4 +1,4 @@
-import { ImageryLayer, WebMapServiceImageryProvider } from "cesium";
+import { ImageryLayer, WebMapServiceImageryProvider, UrlTemplateImageryProvider, WebMercatorTilingScheme } from "cesium";
 const TIANDITU_KEY = "3d493a007db2b9a4cc7e62d137b8e2e5";
 const TIANDITU_OPTION = {
 	layers: "tdtBasicLayer", // 要包含的层，用逗号分隔。
@@ -7,6 +7,7 @@ const TIANDITU_OPTION = {
 	format: "tiles",
 	tileMatrixSetID: "GoogleMapsCompatible",
 	parameters: {},
+	crs: "EPSG:4326",
 };
 const TIANDITU_LAYER_OPTION = {
 	show: true,
@@ -29,6 +30,21 @@ export function createTiandituImageryProvider(type, layer) {
 	});
 }
 /**
+ * @description: 创建天地图URL模板的图像
+ * @param {*} type
+ * @return {imageryProvider}
+ */
+export function createTdtUrlTemplateImageryProvider(type) {
+	// @see http://cesium.xin/cesium/cn/Documentation1.95/UrlTemplateImageryProvider.html?classFilter=UrlTemplateImageryProvider
+	return new UrlTemplateImageryProvider({
+		url: `http://t{s}.tianditu.gov.cn/DataServer?T=${type}&x={x}&y={y}&l={z}&tk=${TIANDITU_KEY}`,
+		subdomains: "01234567",
+		// @see http://cesium.xin/cesium/cn/Documentation1.95/WebMercatorTilingScheme.html?classFilter=WebMercatorTilingScheme
+		tilingScheme: new WebMercatorTilingScheme(), // 平铺方案
+		maximumLevel: 18,
+	});
+}
+/**
  * @description: 获取天地图图层
  * @param {string} type
  * @param {string} layer
@@ -44,24 +60,24 @@ export function createTiandituLayer(type, layer) {
 	);
 }
 export function getTiandituLayers() {
-	// 矢量底图
-	const vecMapLayer = createTiandituImageryProvider("vec_w", "vec");
-	// 矢量标注
-	const cvaMapLayer = createTiandituImageryProvider("cva_w", "cva");
-	// 影像底图
-	const imgMapLayer = createTiandituImageryProvider("img_w", "img");
-	// 影像标注
-	const ciaMapLayer = createTiandituImageryProvider("cia_w", "cia");
-	// 地形渲染
-	const terMapLayer = createTiandituImageryProvider("ter_w", "ter");
-	// 地形标注
-	const ctaMapLayer = createTiandituImageryProvider("cta_w", "cta");
+	// // 矢量底图
+	// const vecMapLayer = createTiandituImageryProvider("vec_w", "vec");
+	// // 矢量标注
+	// const cvaMapLayer = createTiandituImageryProvider("cva_w", "cva");
+	// // 影像底图
+	// const imgMapLayer = createTiandituImageryProvider("img_w", "img");
+	// // 影像标注
+	// const ciaMapLayer = createTiandituImageryProvider("cia_w", "cia");
+	// // 地形渲染
+	// const terMapLayer = createTiandituImageryProvider("ter_w", "ter");
+	// // 地形标注
+	// const ctaMapLayer = createTiandituImageryProvider("cta_w", "cta");
+	// 影像
+	const img3dMapLayer = createTdtUrlTemplateImageryProvider("img_w");
+	// 标注
+	const cia3dMapLayer = createTdtUrlTemplateImageryProvider("cia_w");
 	return {
-		vecMapLayer,
-		cvaMapLayer,
-		imgMapLayer,
-		ciaMapLayer,
-		terMapLayer,
-		ctaMapLayer,
+		img3dMapLayer,
+		cia3dMapLayer,
 	};
 }
--- packages/vue3-cesium-study/src/pages/home/index.vue
+++ packages/vue3-cesium-study/src/pages/home/index.vue
@@ -3,35 +3,77 @@
  * @Author: 曾茹菁
  * @Date: 2022-07-07 09:59:44
  * @LastEditors: yomuki
- * @LastEditTime: 2022-09-23 16:39:58
+ * @LastEditTime: 2022-09-26 16:10:19
 -->
 <template>
 	<div id="map" ref="mapEl"></div>
 </template>
 
 <script setup>
-import { Viewer, createWorldTerrain } from "cesium";
+import { Viewer, createWorldTerrain, Camera, Rectangle, Cartesian3, GeoJsonDataSource, Color } from "cesium";
 import { onMounted, ref } from "vue";
 import { getTiandituLayers } from "@/libs/cesiumHelper";
+const geojson = require("@/assets/geojson/pinghu.json");
 const mapEl = ref(null);
+/**
+ * @description: 初始化Cesium
+ */
+function initCesium() {
+	// Camera.DEFAULT_VIEW_RECTANGLE = Rectangle.fromDegrees(90, -20, 110, 90); // 默认中国上空
+	const pinghuLog = 121.01606, // 平湖经度
+		pinghuLat = 30.67585; // 平湖纬度
+	Camera.DEFAULT_VIEW_RECTANGLE = Rectangle.fromDegrees(pinghuLog - 8, pinghuLat - 8, pinghuLog + 8, pinghuLat + 8); //
+}
 function initMap() {
 	window.mapView = new Viewer("map", {
 		infoBox: false, // 关闭infobox
-		geocoder: true, // 右上工具 - 查找位置
+		geocoder: false, // 右上工具 - 查找位置
 		homeButton: true, //  右上工具 - 返回初始镜头
 		sceneModePicker: true, // 右上工具 - 选择视角模式 2D/3D
 		baseLayerPicker: false, // 右上工具 - 图形选择器。选择地图服务和地形服务
-		navigationHelpButton: true, // 右上工具 - 帮助导航
-		animation: true, // 左下工具 - 动画工具。控制视图播放速度
-		timeline: true, // 正下工具 - 时间线。指示当前时间，提供时间跳转。
+		navigationHelpButton: false, // 右上工具 - 帮助导航
+		animation: false, // 左下工具 - 动画工具。控制视图播放速度
+		timeline: false, // 正下工具 - 时间线。指示当前时间，提供时间跳转。
 		fullscreenButton: true, // 右下工具 - 全屏按钮
 		terrainProvider: createWorldTerrain(),
 	});
-	const { ciaMapLayer } = getTiandituLayers();
-	window.mapView.imageryLayers.addImageryProvider(ciaMapLayer); //添加到cesium图层上
+	window.mapView._cesiumWidget._creditContainer.style.display = "none"; //去除cesium版权信息
+	const { img3dMapLayer, cia3dMapLayer } = getTiandituLayers();
+	window.mapView.imageryLayers.addImageryProvider(img3dMapLayer); //添加到cesium图层上
+	window.mapView.imageryLayers.addImageryProvider(cia3dMapLayer);
 }
+function useSetView() {
+	// @see http://cesium.xin/cesium/cn/Documentation1.95/Camera.html
+	window.mapView.camera.setView({
+		destination: Cartesian3.fromDegrees(121.01606, 30.67585, 15000.0),
+	});
+}
+function useFlyto() {
+	window.mapView.camera.flyTo({
+		destination: Cartesian3.fromDegrees(121.01606, 30.67585, 15000.0),
+		complete: () => {
+			console.log("fly ok");
+		},
+	});
+}
+function useGeojsonDataInMap() {
+	// @see http://cesium.xin/cesium/cn/Documentation1.95/GeoJsonDataSource.html
+	GeoJsonDataSource.load(geojson, {
+		// @see http://cesium.xin/cesium/cn/Documentation1.95/Color.html
+		stroke: Color.PALETURQUOISE,
+		strokeWidth: 8,
+		fill: Color.fromCssColorString("rgba(36,129,252,.2)"),
+	}).then((dataSource) => {
+		// @see http://cesium.xin/cesium/cn/Documentation1.95/DataSourceCollection.html
+		window.mapView.dataSources.add(dataSource);
+	});
+}
+// function initCamera() {}
+initCesium();
 onMounted(() => {
 	initMap();
+	useSetView();
+	useGeojsonDataInMap();
 });
 </script>
 
